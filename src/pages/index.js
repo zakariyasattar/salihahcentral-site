1914416174e0bd63e4c8d4a4a6e11e45395a7f72
Zakariya Sattar <38594399+zakariyasattar@users.noreply.github.com>
2018-05-27 04:37:46 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,9 @@ import "../assets/css/main.css";
 
 import Header from "../components/Header";
 import About from "../components/About";
-import Children from "../components/Children";
 import Calendar from "../components/calender";
 import Footer from "../components/Footer";
 import WLearning from "../components/communityprograms.js";
-import Talks from "../components/talks.js";
 
 
 const IndexPage = () => (
