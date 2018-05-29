c97e054253adcb6dc022da1d7da840c2a9da878d
Zakariya Sattar <zakariyasattar03@gmail.com>
2018-05-29 14:04:31 -0500

    minor changes to fonts, text, etc.

--- src/pages/staff.js
+++ src/pages/staff.js
@@ -43,8 +43,8 @@ const Staff = () => (
   </div>
 
 <body>
-<font color="white">
-    <h4>Director Amina Sayeed</h4>
+<font color="">
+    <h4><tab1></tab1>Director Amina Sayeed</h4>
 </font>
 
 <p>
@@ -52,33 +52,31 @@ const Staff = () => (
 </p>
 <br />
 
-<h4>Amena Ahmed</h4>
+<h4><tab1></tab1>Amena Ahmed</h4>
 
 <p> Amena Ahmed completed her bachelor’s in elementary education from Northeastern Illinois University. She also graduated from the Darse Nizami Alima Program at Darul Uloom Chicago, additionally reaching the culmination of her memorization of the Holy Quran at the same institute under the tutelage of senior scholars. Raised in the North Chicago area, she continues to serve her community as a kindergarten teacher at a local early childhood program and Hifdh-al-Quran teacher for students pursuing memorization afterschool. She also teaches Islamic studies classes for adults and teens.
 </p>
 <br />
 
-<h4>Amina Elmahdy</h4>
+<h4><tab1></tab1>Amina Elmahdy</h4>
 
 <p> Amina Elmahdy has a master’s degree in Marriage and Family Therapy and works as a therapist in Chicago. She is also a gifted education teacher for Northwestern University’s Center for Talent Development. Amina is in her 6th  year of study in pursuant of an Alima degree. She is a teacher of Arabic grammar, morphology and hadith. Additionally, Amina volunteers in the community to design curricula for the Weekend Bees Sunday School, and helps facilitate other community events and speaking opportunities. Amina lives in the Chicago area with her husband and four children.
 </p>
 <br/>
-    <tab1><h4>
-      Iram Shaikh
-    </h4></tab1>
+    <h4><tab1></tab1>Iram Shaikh</h4>
 
     <p>Iram Shaikh was born and raised in Chicago, Illinois. She began her Islamic studies at Jamiatus Salihat, and later completed her studies at Darul Uloom Chicago from which she obtained her Alimah degree under illustrious scholars that have studied throughout the Indian Subcontinent. She graduated with a focus in Arabic grammar, Hadith and Qur’an commentary, and Hanafi Fiqh. Upon completion of her Alimah degree she served the community through establishing and teaching introductory level Tajweed and Fiqh classes for young adult females and organizing weekly sisters’ Halaqas in the Chicagoland area. She went on to complete her B.A. in Urban Elementary Education from the University of Illinois at Chicago. Currently, she is a full time teacher at a local Islamic school teaching middle school Islamic Studies, Islamic History, and Qur’an.
     </p>
     <br />
 
-    <tab1><h4>Sana Mohiuddin</h4></tab1>
+    <h4><tab1></tab1>Sana Mohiuddin</h4>
 
     <p>By profession, Mu`allima Sana is a marital and family therapist.  She has a Masters of Arts in Family Counseling at Northeastern Illinois University and has a Bachelor of Arts from Texas A&M University, majoring in History and minoring in Comparative Religions.Mu`allima Sana’s drive to serve her community began well before her undergraduate studies, organizing local and national programs that aided in the social and spiritual well-being of adolescent youth,culminating in organizing and advising two national conferences that hosted well over ten-thousand participants.
     Furthermore, Mu`allima Sana has remained involved in local programs, including as instructor, counselor, facilitator, and speaker at a variety of programs. Her desire to broaden her perspective on human behavior and family dynamics has led her to study spiritual development under local and international scholars, taking her on travels that have included Damascus, Syria and Lusaka, Zambia.  It was in Zambia that Mu`allima Sana received ijazah, or license to teach, from Jaamia Zainab in the     traditional sciences, including fiqh, hadith, tafsir, usool, sarf, nahw, and balagha. Through her studies and her community involvement, Mu`allima Sana brings a unique holistic approach to both counseling and teaching.  She is teaching online courses in tafsir for Saliha Central and Jaamia Zainab, as well as courses for the Milwaukee community, where she resides with her husband and three children.
     </p>
     <br />
 
-    <h4>Maryah Qureshi</h4>
+    <h4><tab1></tab1>Maryah Qureshi</h4>
     <p>
     Maryah Qureshi is a third year student in Salihah Central's distance alimah program. By profession she is a marriage and family therapist who trains new clinicians in Chicago and has a limited private practice in Lincolnwood, IL. Maryah is an advisor for Sisters Steppin' Up, a youth group for Muslim girls in the Chicagoland area. She completed her BA in Economics and in Public Policy Studies at University of Chicago, and earned her MS in Marital & Family Therapy from Northwestern University. Maryah was born and raised in Chicago and continues to live with her family on the north side of the city.
 
@@ -86,7 +84,7 @@ const Staff = () => (
     <br />
 
 
-    <h4>Sakina Syed</h4>
+    <h4><tab1></tab1>Sakina Syed</h4>
     <p>
     Born in Dubai (UAE) and raised in Chicago, 'Alimah Syeda Sakina began attending the Institute of Islamic Education (IIE) in Elgin, IL in 2002 and graduated from their 'Alimah program in 2009. During this time, she studied under the tutelage of her respected teachers gaining degrees (ijazat) in sciences including tafsir, hadith, and fiqh. 'Alimah Syeda Sakina has served as a teacher at Islamic Foundation School (IFS) in Villa Park, IL. 'Alimah Sakina also teaches Qur'an, tafsir, and Arabic classes in-person and online. She holds halaqahs for women and girls across the greater Chicagoland area. She completed her undergraduate degree from Northeastern Illinois University with a summa cum laude in English secondary education with endorsements to teach middle school Language Arts and Social Studies, and currently teaches English at Adlai E. Stevenson high school in Lincolnshire, Illinois. Alimah Sakina lives in the Chicago area with her husband and three children.
     </p>
