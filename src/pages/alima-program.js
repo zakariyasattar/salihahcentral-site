f76aacf4d4c26ae0373fef39d20f7f419d9b0169
Zakariya Sattar <zakariyasattar03@gmail.com>
2018-05-29 14:14:56 -0500

    minor changes to fonts, text, etc.

--- src/pages/alima-program.js
+++ src/pages/alima-program.js
@@ -5,14 +5,17 @@ const Alima = () => (
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
   <br />
   <h1><font size="+25">Alima Program</font></h1>
+  <a href="/#communityprograms"><tab1><button>Go Back To The Homepage</button></tab1></a>
   <br />
-  <font color="papayawhip">
-  <p><font size="+3">The Salihah Central Alima Program is an in-depth course of study that covers the foundations and branches of Islam. The program follows the traditional Alima syllabus with the additional convenience of teachers and peers close to home. Classes
+
+  <h4><tab1>What is it?</tab1></h4>
+
+  <p>The Salihah Central Alima Program is an in-depth course of study that covers the foundations and branches of Islam. The program follows the traditional Alima syllabus with the additional convenience of teachers and peers close to home. Classes
    in year one start at the basic level, developing a skillset and knowledge base within each student to pursue the study of essential Arabic texts in subsequent years.
-While focusing on the study of ‘ilm, the program also includes periodic, spiritually nurturing talks by Anse Amina. The program’s goal is for students in this cohort stay together through the next few years and insha’Allah graduate together with an Alima degree. We offer a full range of subjects including Qur’anic Tafseer, Hadith, Fiqh and Arabic.</font></p>
-</font>
+While focusing on the study of ‘ilm, the program also includes periodic, spiritually nurturing talks by Anse Amina. The program’s goal is for students in this cohort stay together through the next few years and insha’Allah graduate together with an Alima degree. We offer a full range of subjects including Qur’anic Tafseer, Hadith, Fiqh and Arabic.</p>
 <br />
-<a href="/#communityprograms"><tab1><button>Go Back To The Homepage</button></tab1></a>
+
+
   </div>
 );
 
