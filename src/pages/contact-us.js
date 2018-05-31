bf4a76beeb51af68a4172cf7ceb545d4f9eef6ae
Zakariya Sattar <zakariyasattar03@gmail.com>
2018-05-30 21:44:27 -0500

    contact-us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -5,30 +5,24 @@ import Ribbon from "../components/Ribbon";
 const ExampleForm = () => (
 <div>
 <Ribbon/>
-  <Form render={({
-    submitForm
-  }) => (
-    <form onSubmit={submitForm}>
-    <br/>
-    <br/>
-    <br/>
-      <tab1></tab1><Text field="firstName" placeholder='First Name (required)' />
-      <br/>
-      <br/>
-      <tab1></tab1><Text field="lastName" placeholder='Last Name (required)' />
-      <br/>
-      <br/>
-      <tab1></tab1><Text field="email" placeholder='Email (required)' />
-      <br/>
-      <br/>
-      <font color="white">
-      <tab1></tab1><span>Message:</span>
-      </font>
-
-      <tab6></tab6><TextArea field="message" />
-      <tab1></tab1><button type="submit">Submit</button>
-    </form>
-  )} />
+              <form method="post" action="../pages/#action_page.php">
+            <div className="field half first">
+              <label htmlFor="name"><font color="powderblue"><tab1></tab1>Name</font></label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email"><font color="powderblue"><tab1></tab1>Email</font></label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message"><font color="powderblue"><tab1></tab1>Message</font></label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <br/>
+            <ul className="actions">
+              <li><tab1></tab1><input type="submit" value="Send Message" className="special" /></li>
+            </ul>
+        </form>
 </div>
 )
 
