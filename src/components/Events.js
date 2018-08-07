1651e1754bfb71d4d62056ad1e724ee9151acd6e
Zakariya Sattar <zakariyasattar03@gmail.com>
2018-08-07 18:02:37 -0500

    add events section

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,38 +1,13 @@
 import React from "react";
 
+import Event01 from "../assets/images/magnetsofmercyevent.jpeg";
+<link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet"/>
+
 const Events = () => (
-<section id="events">
-  <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet"/> 
+<section id="events"> 
   <h1>Events</h1>
-
-  <div class="slideshow-container">
-
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="monkey.jpg" />
-  <div class="text">Caption Text</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="img_snow_wide.jpg" />
-  <div class="text">Caption Two</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="img_mountains_wide.jpg" />
-  <div class="text">Caption Three</div>
-</div>
-
-</div>
-<br/>
-
-<div>
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-</div>
+  <div class="header-bar waypoint" ></div>
+  <img className="event" src={Event01} alt="" />
 
 
 </section>
