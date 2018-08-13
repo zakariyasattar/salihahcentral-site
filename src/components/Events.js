2602cf356c2d9c80166b9981a270e96f81564f18
Zakariya Sattar <zakariyasattar03@gmail.com>
2018-08-12 20:51:34 -0500

    minor changes to events section

--- src/components/Events.js
+++ src/components/Events.js
@@ -8,7 +8,6 @@ const Events = () => (
 <section id="events"> 
   <h1>Events</h1>
   <div class="header-bar waypoint" ></div>
-  <tab1></tab1>
   <a href="https://docs.google.com/document/d/1W4b_Qkd0y-0HGBvGGJ-86mUZIreNl6yQUIxPUm_V6xA/edit#heading=h.gjdgxs" target="_blank"><img className="event" src={Event02} alt="" /></a>
   <a href="https://docs.google.com/forms/d/e/1FAIpQLSeU5GgUTsAZskWq_qRN8DzEtK8eKeaK6YB_BEDb5RqT54uqjw/viewform?c=0&w=1" target="_blank"><img className="event" src={Event03} alt="" /></a>
 
