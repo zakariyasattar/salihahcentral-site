498c20dc79a68e35d5aa17962287955cc518038c
Zakariya Sattar <zakariyasattar03@gmail.com>
2020-04-06 15:23:09 -0500

    test

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":630},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
