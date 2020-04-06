054295b119b14adde1486f04b9b2704b5e1cf038
Zakariya Sattar <zakariyasattar03@gmail.com>
2020-04-06 17:33:16 -0500

    add seerah class

--- src/pages/halaqas.js
+++ src/pages/halaqas.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import seerah_class from "../assets/images/programs/Seerah_class.jpg";
+import convert_circle from "../assets/images/programs/Convert_Circle.jpg"
+
 const halaqas = () => (
 <div className="halaqas">
 <link rel="stylesheet" type="text/css" href="main.css"/>
@@ -44,28 +47,20 @@ const halaqas = () => (
 
 <br/>
 
-<h3 style={{
-  color: 'white'
-}}><tab1></tab1>Weeknight ‘Me Time’</h3>
-  <p>
-  Salihah Central strives to teach the seerah to women in order to remove any doubt concerning his authenticity, instill and strengthen the love of Prophet Muhammad (S) in the hearts of Muslims, to gain insight into pragmatic Islam, to strengthen the love of the companions in one's heart, and to unlock the wealth of knowledge there is regarding this topic.
-
-  Allah has commanded us to know the Prophet Muhammad and what better way to do this than to study seerah under qualified teachers. Studying seerah is an obligation that has put upon us as stated in the Holy Quran and Hadith:,
-
-  Indeed in the Messenger of Allah (Muhammad SAW) you have a good example to follow for him who hopes in (the Meeting with) Allah and the Last Day and remembers Allah much. Qur’an [33:21]
-
-  Anas ibn Malik reported: The Messenger of Allah, peace and blessings be upon him, said, “None of you have faith until I am more beloved to him than his children, his father, and all of the people.”
-
-  <tab1>(Source: Ṣaḥīḥ al-Bukhārī 15, Ṣaḥīḥ Muslim 44)</tab1>
-
-  <tab1></tab1>Information on this class is available through the sacredlearning.org or by emailing salihah.central@gmail.com.
-  </p>
-
 <br/>
 
-<font color="white">
-  <span>Click here for a list of current schedules, topics and teachers</span>
-</font>
+<img style={{display: "inline-block", width: "45vw", height: "50vh", padding: "35px"}}src={seerah_class}></img>
+
+<div style={{float: "right", display: "inline-block", width: "50vw", paddingTop: "35px"}}>
+  <span style={{color: "white", fontSize: "15px", position: "relative"}}>Seerah Class</span>
+  <br/>
+  Salihah Central strives to teach the seerah to women in order to remove any doubt concerning his authenticity, instill and strengthen the love of Prophet Muhammad (S) in the hearts of Muslims, to gain insight into pragmatic Islam, to strengthen the love of the companions in one's heart, and to unlock the wealth of knowledge there is regarding this topic.
+  Allah has commanded us to know the Prophet Muhammad and what better way to do this than to study seerah under qualified teachers. Studying seerah is an obligation that has put upon us as stated in the Holy Quran and Hadith:,
+  Indeed in the Messenger of Allah (Muhammad SAW) you have a good example to follow for him who hopes in (the Meeting with) Allah and the Last Day and remembers Allah much. Qur’an [33:21]
+  Anas ibn Malik reported: The Messenger of Allah, peace and blessings be upon him, said, “None of you have faith until I am more beloved to him than his children, his father, and all of the people.”
+  <tab1>(Source: Ṣaḥīḥ al-Bukhārī 15, Ṣaḥīḥ Muslim 44)</tab1>
+  <tab1></tab1>Information on this class is available through the sacredlearning.org or by emailing salihah.central@gmail.com.
+</div>
 
 </div>
 )
